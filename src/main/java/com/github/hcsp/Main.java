acc112592a48aa399e76ecc4bb453a1f13f8274c
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-12-06 18:17:10 +0800

    重构并且创建数据库

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,4 @@
 package com.github.hcsp;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,28 +8,67 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Set<String> processedLinks = new HashSet<>();
-        List<String> linkPool = new ArrayList<>();
-        linkPool.add("https://sina.cn/");
 
-        while (!linkPool.isEmpty()) {
+   // private static final String UserName = "root";
+   // private static final String Password = "82468aliran";
+    private static List<String>  loadUrlsFromDatabase(Connection connection,String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        }
+        return results;
+    }
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:tcp:/Users/aliran/FirstProject-crawler/news", "root", "82468aliran");
+        while (true){
+            List<String> linkPool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
+            if (linkPool.isEmpty()) {
+                break;
+            }
             String link = linkPool.remove(linkPool.size() - 1);
-            if (processedLinks.contains(link) || !isInterestingLink(link)) {
+            insertLinkIntoDatabase(connection, "delete from LINKS_TO_BE_PROCESSED where link = ? ", link);
+            if (isLinkProcessed(connection,link)) {
                 continue;
             }
-            Document doc = httpGetAndParseHtml(link);
-            storeIntoDatabaseIfItIsNewsPage(doc);
-            selectlinks(doc, linkPool);
-            processedLinks.add(link);
+            if( isInterestingLink(link)){
+                Document doc = httpGetAndParseHtml(link);
+                parseUrlFromPageAndStoreIntoDatabase(doc, connection);
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                insertLinkIntoDatabase(connection, "insert into LINKS_ALREADY_PROCESSED (link)values (?)", link);
+            }
+        }
+    }
+    private static void parseUrlFromPageAndStoreIntoDatabase(Document doc, Connection connection) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            insertLinkIntoDatabase(connection, "insert into LINKS_TO_BE_PROCESSED (link)values (?)", href);
+        }
+    }
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link =?")) {
+            statement.setString(1, link);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void insertLinkIntoDatabase(Connection connection, String sql, String link) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
         }
     }
     private static Document httpGetAndParseHtml (String link) throws IOException{
@@ -51,7 +89,6 @@ public class Main {
             private static void selectlinks(Document doc, List<String> linkPool) {
                 doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
             }
-
             private static void storeIntoDatabaseIfItIsNewsPage (Document doc){
                 ArrayList<Element> articleTags = doc.select("article");
                 if (!articleTags.isEmpty()) {
